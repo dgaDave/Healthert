8fe181dc557d5cd08d19d72e2098ee50d530091b
AngelRamos27 <l20121028@morelia.tecnm.mx>
2023-07-13 21:28:35 -0600

    Se refactorizó el código de AgregarSaludActivity

--- app/src/main/java/com/example/healthert/AgregarSaludActivity.kt
+++ app/src/main/java/com/example/healthert/AgregarSaludActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import com.example.healthert.databinding.ActivityAgregarSaludBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -14,56 +13,36 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 
+
 class AgregarSaludActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAgregarSaludBinding
     private lateinit var registrarButton: Button
     private val db = Firebase.firestore
     private var storageRef = Firebase.storage.reference
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        iniciarElementosVisuales()
+        registrarButton.setOnClickListener {
+            if (validarCampos()) obtenerDatosRegistro()
+        }
+        setContentView(binding.root)
+    }
+
+    //Inicializa variables de la interfaz
+    private fun iniciarElementosVisuales() {
         binding = ActivityAgregarSaludBinding.inflate(layoutInflater)
+        registrarButton = binding.registrarBoton
         val sexos = resources.getStringArray(R.array.sexosStrings)
         val adapter = ArrayAdapter(this, R.layout.list_item, sexos)
         with(binding.autoCompleteTextView) {
             setAdapter(adapter)
         }
-
-        registrarButton = binding.registrarBoton
-
-        registrarButton.setOnClickListener {
-            if (validar()) {
-                setContentView(R.layout.loading_layout)
-                registrarPaciente(
-                    intent.getStringExtra("nombre").toString(),
-                    intent.getStringExtra("apellidoP").toString(),
-                    intent.getStringExtra("apellidoM").toString(),
-                    intent.getStringExtra("uri").toString(),
-                    FirebaseAuth.getInstance().uid.toString(),
-                    binding.edadEditText.text.toString().toInt(),
-                    binding.alturaEditText.text.toString().toInt(),
-                    binding.pesoEditText.text.toString().toInt(),
-                    binding.autoCompleteTextView.text.toString(),
-                    binding.alergiasEditText.text.toString(),
-                    binding.padecimientosEditText.text.toString(),
-                    binding.curpEditText.text.toString()
-                    )
-            }
-        }
-
-
-
-
-
-
-
-
-
-        setContentView(binding.root)
     }
 
-    fun validar(): Boolean {
+
+    //Valida que los campos no estén vacios
+    private fun validarCampos(): Boolean {
         var i = 0
         if (binding.edadEditText.text.toString().isEmpty()) {
             binding.edadTextField.error = "El campo no puede estar vacio"
@@ -108,8 +87,32 @@ class AgregarSaludActivity : AppCompatActivity() {
         }
     }
 
+    //Se obtenienen los datos de los campos y se mandan a la función del registro
+    //Se creó la función para ahorrar espacio en el método principal
+    private fun obtenerDatosRegistro() {
+        muestraPantallaCarga()
+        registrarPaciente(
+            intent.getStringExtra("nombre").toString(),
+            intent.getStringExtra("apellidoP").toString(),
+            intent.getStringExtra("apellidoM").toString(),
+            intent.getStringExtra("uri").toString(),
+            FirebaseAuth.getInstance().uid.toString(),
+            binding.edadEditText.text.toString().toInt(),
+            binding.alturaEditText.text.toString().toInt(),
+            binding.pesoEditText.text.toString().toInt(),
+            binding.autoCompleteTextView.text.toString(),
+            binding.alergiasEditText.text.toString(),
+            binding.padecimientosEditText.text.toString(),
+            binding.curpEditText.text.toString()
+        )
+    }
 
-    fun registrarPaciente(
+    private fun muestraPantallaCarga() {
+        setContentView(R.layout.loading_layout)
+    }
+
+    //Función para registrar al paciente, esta manda a llamar la función que inserta los datos en la DB
+    private fun registrarPaciente(
         nombre: String,
         apellidoP: String,
         apellidoM: String,
@@ -123,6 +126,7 @@ class AgregarSaludActivity : AppCompatActivity() {
         padecimientos: String,
         curp: String
     ) {
+
         val nombrec = hashMapOf(
             "nombres" to nombre,
             "apellidoP" to apellidoP,
@@ -139,26 +143,34 @@ class AgregarSaludActivity : AppCompatActivity() {
             "padecimientos" to padecimientos,
             "curp" to curp
         )
+        insertaDB(paciente, curp, uri)
+
+    }
+
+    //Función para registrar los datos en la base de datos remota
+    private fun insertaDB(paciente: Map<String, Any>, curp: String, uri: String) {
         db.collection("users")
             .document(FirebaseAuth.getInstance().uid.toString() + curp)
             .set(paciente).addOnSuccessListener {
-                var file = Uri.parse(uri)
+                val file = Uri.parse(uri)
                 val imgsRef =
                     storageRef.child("images/" + FirebaseAuth.getInstance().uid.toString() + curp)
-                var uploadTask = imgsRef.putFile(file)
+                val uploadTask = imgsRef.putFile(file)
 
                 uploadTask.addOnFailureListener {
-                    Toast.makeText(this, "Valio verga", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "No se pudo registrar en el sistema", Toast.LENGTH_LONG)
+                        .show()
                 }.addOnSuccessListener {
-                    val intent=Intent(this,VincularActivity::class.java)
+                    val intent = Intent(this, VincularActivity::class.java)
                     intent.putExtra("documento", FirebaseAuth.getInstance().uid.toString() + curp)
                     startActivity(intent)
                     finishAffinity()
                 }
 
             }.addOnFailureListener {
-                Toast.makeText(this, "Valio verga", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "No se pudo registrar en el sistema", Toast.LENGTH_LONG).show()
 
             }
-    } 
+
+    }
 }

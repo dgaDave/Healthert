a21b4213a489713bfd768975fbb186d35081eb45
DgaDave <dgaduff@gmail.com>
2023-07-17 14:22:17 -0600

    Se hicieron cambios rapidos de rendimiento

--- app/src/main/java/com/example/healthert/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/healthert/ui/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
-import com.example.healthert.PacientesAdapter
 import com.example.healthert.PacientesSaludAdapter
 import com.example.healthert.databinding.FragmentHomeBinding
 import com.example.healthert.ui.dashboard.Paciente
--- app/src/main/java/com/example/healthert/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/healthert/ui/notifications/NotificationsFragment.kt
@@ -12,14 +12,11 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.healthert.AgregarInfoPersonaActivity
 import com.example.healthert.LoginActivity
 import com.example.healthert.databinding.FragmentNotificationsBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.Source
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
@@ -69,7 +66,7 @@ class NotificationsFragment : Fragment() {
 
 
         //Recuperar foto del usuario y guardar en cache
-        var userRef = storageRef.child("images/" + FirebaseAuth.getInstance().uid.toString())
+        val userRef = storageRef.child("images/" + FirebaseAuth.getInstance().uid.toString())
         Glide.with(this).load(userRef).into(imgView)
 
         //Recuperar nombre del usuario
@@ -79,7 +76,7 @@ class NotificationsFragment : Fragment() {
             nombreUsuarioTextView.text =
                 nombrec["nombres"] + " " + nombrec["apellidoP"] + " " + nombrec["apellidoM"]
         }.addOnFailureListener {
-            Toast.makeText(context, "No se pudo recuperar la informacion", Toast.LENGTH_LONG)
+            Toast.makeText(context, "No se pudo recuperar la informacion", Toast.LENGTH_LONG).show()
         }
 
         //Evento para salir de la cuenta
